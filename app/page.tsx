0387c2d63cb0d8497b4e5c0114a87bb8451066bd
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-03-19 06:21:46 +0100

    added a redirect to the dashboard in case of logged in user

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,15 @@
 import Features from "@/components/Features";
 import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+  if (session) redirect("/dashboard");
   return (
     <div className="">
       <Navbar />
