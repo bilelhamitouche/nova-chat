f539f3c95bc1e213c8138ce1f08f23648eabe837
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-03-29 05:17:38 +0100

    changed /dashboard to /chat

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default async function Home() {
   const session = await auth.api.getSession({
     headers: await headers(),
   });
-  if (session) redirect("/dashboard");
+  if (session) redirect("/chat");
   return (
     <div className="">
       <Navbar />
