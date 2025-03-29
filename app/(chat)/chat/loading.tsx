b6db4dbe7266ea5e86cf115fbbfe6fea79dbafad
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-03-30 00:56:53 +0100

    added a loading component to chat

---
+++ app/(chat)/chat/loading.tsx
@@ -0,0 +1,9 @@
+function Loading() {
+  return (
+    <div className="flex justify-center items-center h-full">
+      <span className="loading loading-spinner loading-xl"></span>
+    </div>
+  );
+}
+
+export default Loading;
