e8e702cea3b3741ca9efdb435bc1f9fd0ac703d9
Bilel Hamitouche <bilelhamitouche4@gmail.com>
2025-03-29 05:18:29 +0100

    added a index route for chat

---
+++ app/(chat)/chat/page.tsx
@@ -0,0 +1,12 @@
+function Chat() {
+  return (
+    <div className="flex flex-col gap-2 text-center">
+      <h2 className="text-2xl font-bold text-gray-500">Welcome to NovaChat</h2>
+      <p className="text-base font-medium text-gray-500">
+        Select a chat or start a new one to get messaging
+      </p>
+    </div>
+  );
+}
+
+export default Chat;
